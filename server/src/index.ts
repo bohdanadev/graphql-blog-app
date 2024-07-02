237f3326f53eab9989757426b444b6dfe8c9d125
Bohdana K <bohdana.dev@gmail.com>
2024-07-03 02:32:16 +0300

    Add Prisma, Postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,20 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './schema';
 import { Query } from './resolvers';
+import { PrismaClient, Prisma } from '@prisma/client';
 
+export const prisma = new PrismaClient();
+
+export interface Context {
+    prisma: PrismaClient<
+        Prisma.PrismaClientOptions,
+        never,
+        Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+    >;
+    userInfo: {
+        userId: number;
+    } | null;
+}
 const server = new ApolloServer({
     typeDefs,
     resolvers: {
